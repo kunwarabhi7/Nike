3568cd39a79bb9847cf797d265c93d0372fd94f2
Abhishek <abhishekparmar162@gmail.com>
2023-04-08 22:31:45 +0530

    complete right side of header

--- components/RightHeader.tsx
+++ components/RightHeader.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsSearch} from 'react-icons/bs'
 import {BiShoppingBag} from 'react-icons/bi'
-import {AiOutlineHeart , AiOutlineMenu} from 'react-icons/ai'
+import {SiJordan} from 'react-icons/si'
+import {AiOutlineHeart,AiOutlineClose , AiOutlineMenu} from 'react-icons/ai'
 type Props = {}
 
 const RightHeader = (props: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+
   return (
-    <div className='flex items-center justify-center space-x-2'>
+    <div className='flex relative items-center justify-center space-x-2'>
         <div className='md:flex hidden items-center justify-center bg-gray-200 rounded-full w-24 h-5'>
             <BsSearch size={20} className='px-1'/>
             <input type='text' className=' bg-transparent rounded-full w-16 text-sm pl-2 py-1 focus:outline-none ' placeholder='search' />
@@ -15,7 +22,26 @@ const RightHeader = (props: Props) => {
         <div><BsSearch size={15} className='md:hidden'/></div>
         <div><AiOutlineHeart className='hidden md:flex' /></div>
         <div><BiShoppingBag /></div>
-        <div><AiOutlineMenu className='md:hidden'/></div>
+        <div className='md:hidden '>
+            
+        
+        {isOpen ? '' : <AiOutlineMenu onClick={toggleMenu}  />}
+
+      {isOpen && (
+        <div className="bg-white w-40 border rounded h-screen absolute top-0 -right-5 ">
+          <ul className='flex flex-col space-y-1 text-sm font-semibold'>
+            <AiOutlineClose onClick={toggleMenu} size={15}/>
+            <li>New & Featured</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+            <li>Sale</li>
+            <li>SNKRS</li>
+            <li><SiJordan size={20}/></li>
+          </ul>
+        </div>
+      )}
+        </div>
     </div>
   )
 }
