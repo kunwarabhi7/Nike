a915d3261838bdbeb0ceb9aa4a9b83523c788ad2
Abhishek <abhishekparmar162@gmail.com>
2023-04-08 23:11:19 +0530

    footer complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,8 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div className='bg-black px-2 text-white'>
+    <div className='bg-black px-2 fixed bottom-0 left-0 right-0 text-white'>
+      <div className='md:flex items-center justify-between p-1'>
       <div className='uppercase text-[10px] font-semibold'>
         <p>Find a STORE</p>
         <p>Become a MEMBER</p>
@@ -16,11 +17,11 @@ const Footer = (props: Props) => {
         <p>Send Us Feedback</p>
         <p>STUDENT DISCOUNTS</p>
       </div>
-      <div className='border-b border-gray-700 my-1'/>
       <div  className='flex items-center justify-between p-2 text-[10px] my-1'>
         <p>GET HELP</p>
 <AiOutlinePlus />
       </div>
+
       <div  className='flex items-center justify-between p-2 text-[10px] my-1'>
         <p>ABOUT NIKE</p>
 <AiOutlinePlus />
@@ -31,6 +32,10 @@ const Footer = (props: Props) => {
         <TfiYoutube className='text-gray-400 hover:text-white cursor-pointer'/>
         <ImInstagram className='text-gray-400 hover:text-white cursor-pointer' />
       </div>
+      </div>
+      <div className='md:hidden border-b border-gray-700 my-1'/>
+        <div className='md:flex items-center justify-between'>
+
       <div className='flex text-[8px] items-center justify-start'>
 <HiLocationMarker />
 <p>India</p>
@@ -39,12 +44,13 @@ const Footer = (props: Props) => {
         <p className='text-gray-400'>© 2023 Nike, Inc. All Rights Reserved
 </p>
       </div>
-      <div className='text-[8px] mt-2 pb-2'>
-        <p className='text-gray-400 cursor-pointer hover:text-white'>Guides</p>
+      <div className='text-[8px] mt-2 pb-2 md:flex items-center justify-between space-x-2'>
+        <p className='text-gray-400 cursor-pointer ml-2 md:ml-0 hover:text-white'>Guides</p>
         <p className='text-gray-400 cursor-pointer hover:text-white'>Terms of sale</p>
         <p className='text-gray-400 cursor-pointer hover:text-white'>Terms of use</p>
         <p className='text-gray-400 cursor-pointer hover:text-white'>Nike Privacy Policy</p>
       </div>
+        </div>
     </div>
   )
 }
