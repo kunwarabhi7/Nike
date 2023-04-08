91495c5fb1dc32c3ac436608f37fdfedb8443fea
Abhishek <abhishekparmar162@gmail.com>
2023-04-08 16:00:38 +0530

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,12 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      },fontFamily:{
+        oswald:'Oswald, sans-serif',
+        urbanist:"Urbanist, sans-serif",
+        popins:'Mochiy Pop One, sans-serif'
+         
+      }
     },
   },
   plugins: [],
