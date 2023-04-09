9ca0c1049c4b88815ece30d731c9dabe83e99054
Abhishek <abhishekparmar162@gmail.com>
2023-04-09 15:27:31 +0530

    added arrow functionality

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -4,24 +4,60 @@ import Image2 from "../public/slide-2.png";
 import Image3 from "../public/slide-3.png";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 import Image from "next/image";
 type Props = {};
 
 const HeroBanner = (props: Props) => {
-  return  <Carousel autoPlay={true} infiniteLoop={true} showIndicators={false} showThumbs={false}>
-  <div>
-      <Image alt="image1" src={Image1} />
-      {/* <p className="legend">Legend 1</p> */}
-  </div>
-  <div>
-      <Image alt="image2" src={Image2} />
-      {/* <p className="legend">Legend 2</p> */}
-  </div>
-  <div>
-      <Image alt="image3" src={Image3} />
-      {/* <p className="legend">Legend 3</p> */}
-  </div>
-</Carousel>
+  return (
+    <Carousel
+      className="mt-12 relative"
+      autoPlay={true}
+      infiniteLoop={true}
+      showStatus={false}
+      showArrows={false}
+      showIndicators={false}
+      showThumbs={false}
+      renderArrowPrev={(clickHandler, hasPrev) => {
+        return (
+          <div
+            className='absolute bottom-8 cursor-pointer right-6 z-10'
+            onClick={clickHandler}
+          >
+            <AiFillCaretLeft size={30}/>
+          </div>
+        );
+      }}
+      renderArrowNext={(clickHandler, hasNext) => {
+        return (
+          <div
+            className='absolute bottom-8 cursor-pointer right-1 z-10'
+            onClick={clickHandler}
+          >
+            <AiFillCaretRight  size={30}/>
+          </div>
+        );
+      }}
+    >
+    
+    
+      <div>
+        <Image alt="image1" className="object-cover" src={Image1} />
+        <p className="absolute top-24 md:top-80 left-4">Buy Now</p>
+        
+      </div>
+      <div>
+        <Image alt="image2" src={Image2} />
+        <p className="absolute top-24 md:top-80 left-4">Buy Now</p>
+        
+      </div>
+      <div>
+        <Image alt="image3" src={Image3} />
+        <p className="absolute top-24 md:top-40 left-4">Buy Now</p>
+        
+      </div>
+    </Carousel>
+  );
 };
 
 export default HeroBanner;
