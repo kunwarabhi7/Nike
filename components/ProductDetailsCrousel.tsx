943e1aea1460104c12d12fe1c2335f7827bab3b3
Abhishek <abhishekparmar162@gmail.com>
2023-04-10 22:19:36 +0530

    fix a bug

--- components/ProductDetailsCrousel.tsx
+++ components/ProductDetailsCrousel.tsx
@@ -1,37 +1,61 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
-import p1 from '../public/p1.png'
-import p2 from '../public/p2.png'
-import p3 from '../public/p3.png'
-import p4 from '../public/p4.png'
-import p5 from '../public/p5.png'
-import p6 from '../public/p6.png'
-import p7 from '../public/p7.png'
+import Image from "next/image";
+import React, { useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
-type Props = {}
+const images = [
+  {
+    id: 1,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  },
+  {
+    id: 2,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/2dffeb94-7082-47be-ab56-963e7584ca01/air-force-1-react-shoes-mm8pv3.png",
+  }, {
+    id: 3,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  }, {
+    id: 4,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  }, {
+    id: 5,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  }, {
+    id: 6,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  }, {
+    id: 7,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  }, {
+    id: 8,
+    url: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/53a1614a-a5fd-4bfe-977e-6921451ed50e/air-force-1-react-shoes-mm8pv3.png",
+  },
+];
+
+type Props = {};
 
 const ProductDetailsCrousel = (props: Props) => {
-    const [activeSlide, setActiveSlide] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(0);
   return (
     <div>
-        <div className='text-xl w-full max-w-[360px] h-96 mx-auto sticky top-[50px] my-20'>
-        <Carousel  infiniteLoop autoPlay autoFocus showThumbs={true}>
-                <div>
-                    <Image src={p1} alt='' />
-
-                </div>
-                <div>
-                    <Image src={p2} alt=''/>
-                </div>
-                <div>
-                    <Image src={p3}  alt=''/>
-                </div>
-            </Carousel>
+      <div className=" z-40 text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+        <Carousel
+          className="productCarousel"
+          thumbWidth={60}
+          infiniteLoop={true}
+          showIndicators={false}
+          showStatus={false}
+          
+          autoFocus
+        >
+          {images.map((image, index) =>(
+           
+                <img key={index} alt="image"  src={image.url} />
+           ))}
+        </Carousel>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default ProductDetailsCrousel
+export default ProductDetailsCrousel;
--- pages/shoe/[id].tsx
+++ pages/shoe/[id].tsx
@@ -1,14 +1,19 @@
-import ProductDetailsCrousel from "@/components/ProductDetailsCrousel"
+import ProductDetailsCrousel from "@/components/ProductDetailsCrousel";
 
-type Props = {}
+type Props = {};
 
 const ImageZoom = (props: Props) => {
   return (
- <div className="">
-  <div className=""><ProductDetailsCrousel /></div>
-  <div>right</div>
- </div>   
-  )
-}
+    <div className="w-full md:py-20">
+<div className="flex flex-col md:flex-row md:px-10 gap-[50px] lg:gap-[100px] items-center justify-evenly">
 
-export default ImageZoom
+   <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full">
+    <ProductDetailsCrousel />
+   </div>
+   <div>right</div>
+</div>
+    </div>
+  );
+};
+
+export default ImageZoom;
