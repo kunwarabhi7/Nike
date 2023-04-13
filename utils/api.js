9865af1f92e808090c8633e38f08e2b2d1ccb443
Abhishek <abhishekparmar162@gmail.com>
2023-04-13 15:38:58 +0530

    added react typescript

---
+++ utils/api.js
@@ -0,0 +1,14 @@
+import { API_URL, STRAPI_API_TOKEN } from "./urls";
+
+export const fetchData = async (endpoint) => {
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + STRAPI_API_TOKEN,
+    },
+  };
+  const res = await fetch(`${API_URL}${endpoint}`, options);
+  const data = await res.json();
+
+  return data;
+};
