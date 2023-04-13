0d20a3278e9d0d6e7154ad0aba2e2987240347ec
Abhishek <abhishekparmar162@gmail.com>
2023-04-13 16:31:19 +0530

    get data from backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import FirstComponent from '@/components/FirstComponent';
 import HeroBanner from '@/components/HeroBanner';
 import Products from '@/components/Products';
-
+import { useEffect, useState } from 'react';
+import {fetchDataFromApi} from '../utils/api'
 type Props = {};
 
 const Home = (props: Props) => {
+const [data , setData] =   useState(null)
+
+
+const fetchProduct =async () => {
+  const {data} =  await fetchDataFromApi("/api/products")
+  setData(data)
+}
+useEffect(()=>{
+  fetchProduct()
+},[])
+console.log(data)
+
+
   return (
     <div className=''>
 <HeroBanner />
