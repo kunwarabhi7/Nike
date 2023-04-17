18552b83c71ddba673c9edb0198026df0c130cb5
Abhishek <abhishekparmar162@gmail.com>
2023-04-17 14:12:27 +0530

    added submenu

--- components/SubMenu.tsx
+++ components/SubMenu.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import {fetchDataFromApi} from '../utils/api'
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../utils/api";
+import Link from "next/link";
 
-type Props = {
- 
-}
+type Props = {};
 const subMenuData = [
   { id: 1, name: "Jordan", doc_count: 11 },
   { id: 2, name: "Sneakers", doc_count: 8 },
@@ -13,35 +11,45 @@ const subMenuData = [
 ];
 
 const SubMenu = (props: Props) => {
-const[submenu , setSubmenu] =    useState(false)
-const [categories, SetCategories] = useState(null)
+  const [submenu, setSubmenu] = useState(false);
+  const [categories, SetCategories] = useState(null);
 
+  const fetchCategories = async () => {
+    const { data } = await fetchDataFromApi("/api/categories?populate=*");
+    SetCategories(data);
+  };
 
-const fetchCategories = async () => {
-  const {data} =  await fetchDataFromApi('/api/categories?populate=*')
-  SetCategories(data)
-}
-
-
-useEffect(()=>{
-fetchCategories()
-},[])
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
-    <div className='flex justify-center'>
-      <div  onMouseLeave={()=>setSubmenu(false)}>
-        <button onMouseEnter={()=>setSubmenu(true)}>
-          Sale
-        </button>
-        <ul  className={`absolute right-0 w-40 px-2 bg-gray-100 h-48 py-2  rounded  shadow-xl ${submenu? 'block':'hidden'}`}>
-{categories?.map(({attributes,id})=>(
-  <Link href={`/category/${attributes.slug}`} key={id}>{attributes.name}<span>{attributes.products.data.length}</span></Link>
-))}
+    <div className="flex justify-center">
+      <div onMouseLeave={() => setSubmenu(false)}>
+        <button onMouseEnter={() => setSubmenu(true)}>Sale</button>
+        <ul
+          className={`absolute right-0 w-72 px-2 bg-gray-100 h-56 py-2  rounded  shadow-xl ${
+            submenu ? "block" : "hidden"
+          }`}
+        >
+          {categories?.map(({ attributes, id }) => (
+            <Link href={`/shoe/${attributes.slug}`} key={id}>
+              <div className="flex justify-between ">
+<h1>
+
+              {attributes.name}
+</h1>
+              <span>
+                {attributes.products.data.length}
+              </span>
+              </div>
+                <br />
+            </Link>
+          ))}
         </ul>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default SubMenu
+export default SubMenu;
