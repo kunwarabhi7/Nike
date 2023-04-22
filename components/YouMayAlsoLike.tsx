68c2cfcd419e0e9ba2d3967838a97a9b34629734
Abhishek <abhishekparmar162@gmail.com>
2023-04-22 17:54:12 +0530

    added react typescript

--- components/YouMayAlsoLike.tsx
+++ components/YouMayAlsoLike.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import Products from './Products';
 import Product from './Product';
 import Image from 'next/image';
 
